14a489717c5fe6df230ffce618c57f05837bc933
yanbo0039 <yanbo0039@yeah.net>
2019-07-04 14:33:34 +0800

    面向对象编程—this关键字

---
+++ src/变量/Test_this关键字.java
@@ -0,0 +1,14 @@
+package 变量;
+
+public class Test_this关键字 {
+
+	public static void main(String[] args) {
+
+		Order order = new Order();
+		
+		order.setOrderId(123);
+		
+		System.out.println(order.getOrderId());
+	}
+
+}
