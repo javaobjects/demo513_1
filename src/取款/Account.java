f8687c4f82e490ea14028981683d9e640df433e4
yanbo0039 <yanbo0039@yeah.net>
2019-07-04 08:43:58 +0800

    src///编号 密码 余额 开户行 经办人 身份证 手机号

--- src/取款/Account.java
+++ src/取款/Account.java
@@ -1,5 +1,12 @@
 package 取款;
 
 public class Account {
-
+	//编号 密码 余额 开户行 经办人 身份证 手机号
+	private String id;
+	private String password;
+	private Double money;
+	private String bankId;
+	private int operatorId;
+	private String idCard;
+	private String mobilephone;
 }
