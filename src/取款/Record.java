9d623f86a02117b4a0fc063faa6d6e8ad28020c1
yanbo0039 <yanbo0039@yeah.net>
2019-07-04 08:42:05 +0800

    面向对象编程—什么是类

---
+++ src/取款/Record.java
@@ -0,0 +1,13 @@
+package 取款;
+
+public class Record {
+
+	//编号 密码 余额 开户行 经办人 身份证 手机号
+	private String id;
+	private String password;
+	private Double money;
+	private String bankId;
+	private int operatorId;
+	private String idCard;
+	private String mobilephone;
+}
