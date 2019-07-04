411b58dcfb56c88a7b8367c27473bfaa0fb3e29d
yanbo0039 <yanbo0039@yeah.net>
2019-07-04 14:01:13 +0800

    面向对象编程—匿名对象

---
+++ src/构造器/Test匿名对象.java
@@ -0,0 +1,18 @@
+package 构造器;
+
+import java.util.Date;
+
+public class Test匿名对象 {
+
+	public static void main(String[] args) {
+
+		//输出当前日期 Java的类除咯lang包都要导包
+//		Date date = new Date();
+//		System.out.println(date.toString());
+//		System.out.println(date);
+		
+		System.out.println(new Date().toString());//这就是匿名对象
+		System.out.println(new Date());//这就是匿名对象
+	}
+
+}
